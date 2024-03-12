c3d8f76222e4635ebec077280d668eb90b81d574
yuleetea <yuleetea@gmail.com>
2024-03-12 19:07:11 -0400

    setup db connectivity with frontend

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Dockerized Python application. CI/CD with Jenkins
+to run the application in a development server: flask run --host 0.0.0.0
+
+
--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World! I'm making this change to test the github webhook integration with Jenkins. changed for the 7th time, changed webhook payload. added /github-webhook/ and tried to add port exceptions for http and https"
+# Connect to your MySQL RDS database
+# Replace 'your_host', 'your_username', 'your_password', and 'your_database' with your actual credentials
+conn = mysql.connector.connect(
+    host='terraform-20240312203455523600000003.c7ai8gg087tc.us-east-1.rds.amazonaws.com',
+    port= 3306,
+    user='admin',
+    password='password',
+    database = 'mydb'
+)
 
+cursor = conn.cursor()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+
+    # Insert the user input into the database
+    insert_query = "INSERT INTO mydb.names (name) VALUES (%s)"
+    cursor.execute(insert_query, (name,))
+    conn.commit()
+
+    return render_template('submit.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
