48ea79f5e0b92a110ea6e39d4a7395989c5a62dc
yuleetea <yuleetea@gmail.com>
2024-03-15 01:08:22 -0400

    pytest integration

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,12 +1,19 @@
-# tests/test_routes.py
-
+import pytest
+from flask.testing import FlaskClient
 from app import app
 
-# Example test case
-def test_index():
-    client = app.test_client()
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index(client: FlaskClient):
     response = client.get('/')
-#    assert response.status_code == 200
-#    assert 'Hello, World!' in response.data
-    print("Hello :)")
-    print(response.data)
+    assert response.status_code == 200
+    assert b'<form action="/submit" method="post">' in response.data
+
+def test_submit(client: FlaskClient):
+    response = client.post('/submit', data={'name': 'Test Name'})
+    assert response.status_code == 200
+    assert b'Thank you for submitting the form' in response.data
